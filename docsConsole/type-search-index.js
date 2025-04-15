66518dd229df0bfae7b345ab474a8f720d5efcfd
antkart <ar.kartomyshev@gmail.com>
2025-04-15 17:58:50 +0300

    Add files via upload

---
+++ docsConsole/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"console","l":"Console"},{"p":"console","l":"ConsoleMain"},{"p":"console","l":"ConsoleSearch"}];updateSearchResults();
